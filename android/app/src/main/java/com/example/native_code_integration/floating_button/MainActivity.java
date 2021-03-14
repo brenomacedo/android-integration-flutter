dfffb4429639a2556847c895c929cc99c8bae9e8
brenosekiro <b88660501@gmail.com>
2021-03-13 22:07:29 -0300

    creatingMethods

--- android/app/src/main/java/com/example/native_code_integration/floating_button/MainActivity.java
+++ android/app/src/main/java/com/example/native_code_integration/floating_button/MainActivity.java
@@ -11,5 +11,31 @@ public class MainActivity extends FlutterActivity {
         GeneratedPluginRegistrant.registerWith(this);
 
         MethodChannel channel = new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL);
+        
+        channel.setMethodCallHandler(
+            (call, result) -> {
+                switch(call.method) {
+                    case "create":
+                        ImageView imageView = new ImageView(getApplicationContext());
+                        imageView.setImageResource(R.drawable.plus);
 
+                        FloatWindow.with(getApplicationContext()).setView(imageView)
+                            .setWidth(Screen.width, 0.15f)
+                            .setHeight(Screen.height, 0.15f)
+                            .setX(Screen.width, 0.8f)
+                            .setY(Screen.height, 0.3f)
+                            .setDesktopShow(true)
+                            .build();
+
+                        break;
+                    case "show":
+                        FloatWindow.get().show();
+                        break;
+                    case "hide":
+                        FloatWindow.get().hide();
+                        break;
+                }
+            }
+        );
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,44 @@ class _HomeState extends State<Home> {
 
   static const platform = const  MethodChannel("floating_button");
 
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Floating Button Demo'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("$count", textAlign: TextAlign.center, style: TextStyle(
+              fontSize: 50
+            )),
+            ElevatedButton(
+              child: Text('Create'),
+              onPressed: () {
+                platform.invokeMethod('create');
+              }
+            ),
+            ElevatedButton(
+              child: Text('Show'),
+              onPressed: () {
+                platform.invokeMethod('show');
+              }
+            ),
+            ElevatedButton(
+              child: Text('Hide'),
+              onPressed: () {
+                platform.invokeMethod('hide');
+              }
+            )
+          ],
+        ),
+      ),
     );
   }
 }
